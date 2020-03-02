edd9bd4575094f961f50f53d7ed018460d6ed18d
Catevika <dominique.bello@outlook.fr>
2020-03-02 17:44:50 +0100

    Updates

---
+++ precache-manifest.4fb78dc4898b825241d055ee2f770d42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0390134858a12050521e951ec75c7d31",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "a9d3261bae9a9401780c",
+    "url": "/Random-Quote-Machine/static/css/2.910c6de3.chunk.css"
+  },
+  {
+    "revision": "a1b1daff872d3e0101d3",
+    "url": "/Random-Quote-Machine/static/css/main.8d62cdba.chunk.css"
+  },
+  {
+    "revision": "a9d3261bae9a9401780c",
+    "url": "/Random-Quote-Machine/static/js/2.98953393.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Random-Quote-Machine/static/js/2.98953393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b1daff872d3e0101d3",
+    "url": "/Random-Quote-Machine/static/js/main.7e6d3db5.chunk.js"
+  },
+  {
+    "revision": "b623aee230755d758bae",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.aa521981.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
