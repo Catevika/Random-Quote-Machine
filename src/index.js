6c0580aa0af8f2c86167d66cbe4cf09d6db006e8
Catevika <dominique.bello@outlook.fr>
2020-03-03 10:55:10 +0100

    Revert "Fixed vh mobile"
    This reverts commit 07333c2859985209494e9784acf60d7ea7bd9ccc.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
